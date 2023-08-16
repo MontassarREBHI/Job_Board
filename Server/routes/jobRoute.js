384d3dbe21214c9ca2f02aa25568683cfdb68853
unknown <oussemabenbouali.bouyahia@gmail.com>
2023-08-16 11:55:50 +0100

    uploading pdf files to the server

--- Server/routes/jobRoute.js
+++ Server/routes/jobRoute.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const jobController = require("../controllers/jobController");
 
 router.route("/").post(jobController.addJob).get(jobController.jobList);
-
+router.post("/cvs",jobController.fileUpload,jobController.jobApply)
 module.exports = router;
