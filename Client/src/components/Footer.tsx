efb67518521cacf2a8289ec9889e0ac45088d411
Montassar <montassar.rebhi@fst.utm.tn>
2023-09-23 23:33:41 +0100

    added logo and styling refining

--- Client/src/components/Footer.tsx
+++ Client/src/components/Footer.tsx
@@ -1,3 +1,4 @@
+import { Nav } from "react-bootstrap";
 const Footer = () => {
   return (
     <footer
@@ -12,14 +13,7 @@ const Footer = () => {
         <div className="">
           <p className="flex items-center justify-center">
             <span className="mr-4">Register for free</span>
-            <button
-              type="button"
-              className="inline-block rounded-full border-2 border-neutral-50 px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
-              data-te-ripple-init
-              data-te-ripple-color="light"
-            >
-              Sign up!
-            </button>
+            <Nav.Link>Sign up!</Nav.Link>
           </p>
         </div>
       </div>
